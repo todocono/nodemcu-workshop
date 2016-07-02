6191fbc3575b23125acd7f70c2edd81d9a8760aa
Rodolfo Cossovich <todocono@gmail.com>
2016-07-02 22:46:22 +0800

    first commit

--- ESP-breathe/ESP-breathe.ino
+++ ESP-breathe/ESP-breathe.ino
@@ -2,11 +2,9 @@
   by Rodolfo Cossovich http://rudi.plobot.com
   This example code is in the public domain.
 
-  Motors and battery connected
-  All peripherals as well
+  LED connected 
 */
 
-#include <Servo.h>
 
 #define D0 16     //onboard LED 
 #define D1 5
@@ -26,23 +24,6 @@
 #define trigPin D0
 #define echoPin D1
 
-//pins for ultrasound
-#define PWMA D1
-#define DIRA D3
-#define PWMB D2
-#define DIRB D4
-
-// constants won't change :
-const long TIME = 500;           // interval at which to blink (milliseconds)
-
-
-#define trigPin D0
-#define echoPin SD3
-
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
-
-int pos = 0;    // variable to store the servo position
 
 unsigned long previousMillis = 0;        // will store last time LED was updated
 
@@ -57,44 +38,19 @@ void setup()
   pinMode( D7, OUTPUT);
   pinMode( D5, OUTPUT);
 
-  pinMode( PWMA, OUTPUT);
-  pinMode( DIRA, OUTPUT);
-  pinMode( PWMB, OUTPUT);
-  pinMode( DIRB, OUTPUT);
-
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-
   Serial.println("starting...");
 }
 
 void loop()
 {
-
   breathe();
-
 }
 
-void dance () {
-
-}
 void breathe ( void ) {
   float currentMillis = millis() / 5000.0;
   int value = 512.0 + 512 * sin( currentMillis * 2.0 * PI  );
   analogWrite(D6, value);
   analogWrite(D7, value);
   analogWrite(D5, value);
-}
-
-int ultra() {
-  long duration, distance;
-  digitalWrite(trigPin, LOW);  // Added this line
-  delayMicroseconds(2); // Added this line
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10); // Added this line
-  digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);
-  distance = (duration / 29) ;
-  return (int)distance;
 }
 
